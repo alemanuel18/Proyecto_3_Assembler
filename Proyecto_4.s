5a31a51d37008509f55e47097a9aacb7ca4352c1
div468 <divasjulian2019ig@gmail.com>
2025-06-03 08:04:54 -0600

    Final

--- Proyecto_4.s
+++ Proyecto_4.s
@@ -37,15 +37,15 @@
 .equ RCC_GPIOAEN, 0x00000001                // Bit para habilitar reloj a GPIOA
 .equ RCC_GPIOBEN, 0x00000002                // Bit para habilitar reloj a GPIOB
 .equ RCC_GPIOCEN, 0x00000004                // Bit para habilitar reloj a GPIOC
-.equ BUTTON_BLUE, 0x00002000                // Bit de PC13 (botón azul)
-.equ BUTTON_BLACK,0x00000001                // Bit de PC0 (botón negro)
+.equ BOTON_AZUL, 0x00002000                // Bit de PC13 (botón azul)
+.equ BOTON_NEGRO,0x00000001                // Bit de PC0 (botón negro)
 
 //Delays para las velocidades de los ciclos
 .equ DELAY_LENTO,   50000000                 // Delay para velocidad lenta (2.5 s)
 .equ DELAY_MEDIO, 30000000                 // Delay para velocidad media (1.5 s)
 .equ DELAY_RAPIDO,   3000000                  // Delay para velocidad rápida (0.5 s)
-.equ DEBOUNCE_TIME, 20000                   // Tiempo de rebote para evitar lecturas falsas
-.equ BUTTON_CHECK_INTERVAL, 500             // Intervalo entre verificaciones de botones
+.equ TIEMPO_REBOTE, 20000                   // Tiempo de rebote para evitar lecturas falsas
+.equ INTERVALO_BOTON, 500             // Intervalo entre verificaciones de botones
 
 .syntax unified
 .cpu cortex-m4
@@ -54,13 +54,14 @@
 //Datos a utilizar al principio del programa
 .section .data
 //Se empieza en la velocidad lenta con su respectivo delay
-current_delay: .word DELAY_LENTO             // Retardo actual inicial (lento)
-delay_state:   .word 1                      // Estado de velocidad: 1=lento, 2=medio, 3=rápido
-button_debounce: .word 0                   // Contador de rebote del botón
-last_button_state: .word 0                 // Estado anterior de los botones
+delay_actual: .word DELAY_LENTO             // Retardo actual inicial (lento)
+estado_delay:   .word 1                      // Estado de velocidad: 1=lento, 2=medio, 3=rápido
+contador_rebote: .word 0                   // Contador de rebote del botón
+ultimo_estado_boton: .word 0                 // Estado anterior de los botones
 
 .section .text
 .global main
+//Autor: Marcelo Detlefsen
 main:
 // Habilita el reloj para GPIOA, GPIOB y GPIOC
     LDR r0, =RCC_AHB1ENR //Direccion para habilitar el clock de los puertos A B y C
@@ -83,6 +84,7 @@ main:
 // Pequeño retardo para estabilizar el sistema
     MOV r2, #0x1000
 
+//Alejandro Jerez
 delay1:
     SUBS r2, r2, #1
     BNE delay1
@@ -130,14 +132,15 @@ delay1:
     MOV r1, #0 //Se desactiva el timer
     STR r1, [r0] //Se guarda la configuracion
 
+//Marcelo Detlefsen
 main_loop:
-    BL check_buttons                     // Verifica estado de botones
+    BL revisar_botones                     // Verifica estado de botones
     LDR r5, =GPIOA_ODR //Carga las direcciones de salida del GPIOA
 
 // Paso 1: PA0 encendido
     MOV r0, #(1 << 0)
     STR r0, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 2: PA0 + PA1 + PA8
     MOV r0, #(1 << 0)
@@ -146,11 +149,11 @@ main_loop:
     ORR r6, r0, r1
     ORR r6, r6, r2
     STR r6, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 3: Solo PA8
     STR r2, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 4: PA8 + PA9 + PA4
     MOV r3, #(1 << 9)
@@ -158,34 +161,35 @@ main_loop:
     ORR r6, r2, r3
     ORR r6, r6, r4
     STR r6, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 5: Solo PA4
     STR r4, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 6: PA4 + PA5 + PA6
     MOV r7, #(1 << 5 | 1 << 6)
     ORR r6, r4, r7
     STR r6, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 7: Solo PA6
     MOV r6, #(1 << 6)
     STR r6, [r5]
-    BL responsive_delay
+    BL loop_delay
 
 // Paso 8: Solo PA7
     MOV r7, #(1 << 7)
     STR r7, [r5]
-    BL responsive_delay
+    BL loop_delay
 
     B main_loop
 
-responsive_delay:
+//Autor: Julián Divas
+loop_delay:
 	//Guarda los registros para no perder sus valores
     PUSH {r0-r5, lr}
-    LDR r0, =current_delay
+    LDR r0, =delay_actual
     LDR r0, [r0]	//Se obtiene el valor del delay actual usado para controlar la velocidad (ciclos)
 
     LDR r1, =STK_LOAD //Carga en r1 la dirección en memoria del timer a utilizar
@@ -197,83 +201,91 @@ responsive_delay:
     MOV r2, #0x05	  //Coloca el valor en 0000 0101 (usar el reloj del sistema para el temporizador)
     STR r2, [r1]	  //Se guarda la configuración
 
-delay_loop:
+	BL leer_botones_delay
+
+//Autor: Julián Divas
+leer_botones_delay:
 	//Revisa dos veces que se hayan apachado los botones para poder cambiar de velocidad en un ciclo o entre ciclos
-    BL check_buttons
-    BL check_buttons
+    BL revisar_botones
+    BL revisar_botones
 
 	//carga la dirección para controlar el timer
     LDR r1, =STK_CTRL
     LDR r2, [r1]	//Se obtiene el valor del timer
     ANDS r2, r2, #0x10000        //Se espera a que el timer deje de contar (bit 16 countflag == 1)
-    BEQ delay_loop				//Sigue eseprando a que termine de contar el timer
+    BEQ leer_botones_delay				//Sigue eseprando a que termine de contar el timer
     MOV r2, #0
     STR r2, [r1]		//Si ya terminó de contar se carga el 0 al controlador del timer para detenerlo
     POP {r0-r5, pc}		//Se restauran los valores guardados
-check_buttons:
+
+//Autor: Julián Divas
+revisar_botones:
     PUSH {r0-r5, lr}
     LDR r0, =GPIOC_IDR //Registro de entradas del GPIOC
     LDR r1, [r0]                 // Lee entradas actuales (del GPIOC)
-    LDR r2, =last_button_state	////Carga a R2 la variable del ultimo estado del boton
+    LDR r2, =ultimo_estado_boton	////Carga a R2 la variable del ultimo estado del boton
     LDR r3, [r2]                 // Obtiene el valor del ultimo estado del boton
     STR r1, [r2]                 // Se actualiza el estado de last button state
 
-    LDR r5, =button_debounce //contador de rebote
+    LDR r5, =contador_rebote //contador de rebote
     LDR r4, [r5] //Carga el valor de la cantidad de veces que rebotó
     CMP r4, #0 //Se compara que no hubieran habido rebotes contador de rebote == 0
-    BEQ check_edges  //En caso de que no haya rebote
+    BEQ revisar_flancos_azules  //En caso de que no haya rebote
     SUBS r4, r4, #1 //Si era distinto de 0, le quita 1 para esperar a que no haya rebote
     STR r4, [r5] //guarda el nuevo valor en el contador de rebotes
-    B end_check	//Fin de la funcion
+    B finalizar_revision	//Fin de la funcion
 
-check_edges:
-    TST r3, #BUTTON_BLUE         //Compara si el bit del botón azul estaba activo antes, valor de R3
-    BEQ check_black_edge		//Si no estaba activado antes, revisa otro boton
-    TST r1, #BUTTON_BLUE        // R1 tiene las entradas actuales del GPIOC, lo compara con el boton azul para ver si sigue presionado
-    BNE check_black_edge		//Si sigue presionado no pasa nada y revisa otro botón
+//Autor: Julián Divas
+revisar_flancos_azules:
+    TST r3, #BOTON_AZUL         //Compara si el bit del botón azul estaba activo antes, valor de R3
+    BEQ revisar_flancos_negro		//Si no estaba activado antes, revisa otro boton
+    TST r1, #BOTON_AZUL        // R1 tiene las entradas actuales del GPIOC, lo compara con el boton azul para ver si sigue presionado
+    BNE revisar_flancos_negro		//Si sigue presionado no pasa nada y revisa otro botón
 								//Si si está liberado (se trabaja con flanco de subida)
-    LDR r0, =delay_state		//Retardo utilizado actualmente
+    LDR r0, =estado_delay		//Retardo utilizado actualmente
     LDR r1, [r0]				//CArga el valor del retardo
     CMP r1, #3					//Si ya es la velocidad 3, no se hace nada
-    BEQ set_debounce			//Se reinicia el tiempo de rebote para usar nuevamente el boton
+    BEQ asignar_tiempo_rebote			//Se reinicia el tiempo de rebote para usar nuevamente el boton
     ADD r1, r1, #1				//Si no ha llegado al mmaximo, se aumenta en 1 el delay (para de 1 lento a 2 medio por ejemplo)
     STR r1, [r0]				//Se guarda el cambio en la variable
-    BL update_speed				//Con este cambio conlleva un aumento de velocidad
-    B set_debounce				//Se reinicia el tiempo de rebote para usar nuevamente el boton
+    BL actualizar_velocidad				//Con este cambio conlleva un aumento de velocidad
+    B asignar_tiempo_rebote				//Se reinicia el tiempo de rebote para usar nuevamente el boton
 
-check_black_edge:
-    TST r3, #BUTTON_BLACK	//Compara si el bit del botón negro estaba activo antes, valor de r3
-    BEQ end_check			// Si el bit era 0, osease no estaba presionado, se deja de revisar ya que no hay cambio
-    TST r1, #BUTTON_BLACK	//Se verifica si el botón sigue presionado en caso si se haya apachado
-    BNE end_check			//Si sigue presionado se termimna,
+//Autor: Julián Divas
+revisar_flancos_negro:
+    TST r3, #BOTON_NEGRO	//Compara si el bit del botón negro estaba activo antes, valor de r3
+    BEQ finalizar_revision			// Si el bit era 0, osease no estaba presionado, se deja de revisar ya que no hay cambio
+    TST r1, #BOTON_NEGRO	//Se verifica si el botón sigue presionado en caso si se haya apachado
+    BNE finalizar_revision			//Si sigue presionado se termimna,
 
-    LDR r0, =delay_state	//Si ya no está presionado (es decir detectamos un flanco de bajada)
+    LDR r0, =estado_delay	//Si ya no está presionado (es decir detectamos un flanco de bajada)
     LDR r1, [r0]			//Se carga en r1 el valor del delay actual
     CMP r1, #1				//Se compara para ver si ya está en la velocidad 1 (la más baja)
-    BEQ set_debounce		//Si ya esta en la más baja solo se aplica el tiempo de rebote
+    BEQ asignar_tiempo_rebote		//Si ya esta en la más baja solo se aplica el tiempo de rebote
     SUB r1, r1, #1			//Si no está en la más baja, se baja en una velocidad
     STR r1, [r0]			//Se guarda el cambio
-    BL update_speed			//Se actualiza la velocidad
+    BL actualizar_velocidad			//Se actualiza la velocidad
 
-
-set_debounce:
+//Autor: Julián Divas
+asignar_tiempo_rebote:
 	//Se carga a 0 la variable tiempo de rebote
-    LDR r0, =DEBOUNCE_TIME
+    LDR r0, =TIEMPO_REBOTE
     //Carga el valor definidod al contador de rebote
     STR r0, [r5]
 
-end_check:
+finalizar_revision:
 	//Restaura el estado anterior de los registros y sale de la funcion
     POP {r0-r5, pc}
 
+//Autor: Julián Divas
 //Actualizar la velocidad del patrón
-update_speed:
+actualizar_velocidad:
 	//Se almacenan en una pila los registros para no perder sus valores actuales
     PUSH {r0-r2, lr}
     //Se carga la dirección de memoria del delay actual (tiempo)
-    LDR r0, =current_delay
+    LDR r0, =delay_actual
     //Se carga la direccion de memoria del delay (estado, 1 = lento, 2 = medio, 3 = rapido)
-    LDR r1, =delay_state
+    LDR r1, =estado_delay
     //Se carga el valor del estado del delay en r1
     LDR r1, [r1]
 
@@ -295,12 +307,13 @@ update_speed:
     LDREQ r2, =DELAY_RAPIDO //Si es igual se carga la velocidad correspondiente a rapido
     STREQ r2, [r0]			//Se guarda la configuración
 	//Se actualizan las leds encendidas dependiendo de la velocidad
-    BL update_leds
+    BL actualizar_leds
     //Restaura los valores de los registros originales
     POP {r0-r2, pc}
 
+//Autor: Julián Divas
 //Define que led se encenderá dependiendo del estado de velocidad en el que se encuentre
-update_leds:
+actualizar_leds:
 	//Se guardan los valores de los registros para no perderlos durante la funcion
     PUSH {r0-r2, lr}
     LDR r0, =GPIOB_ODR	//Se carga en R0 las salidas del GPIOO
@@ -308,7 +321,7 @@ update_leds:
     MOV r2, #0
     STR r2, [r0]              //Se guarda la configuración con todos apagados
     //Se obtiene el valor del estado de velocidad actual (1 =lento, 2 =  medio, 3 = rapido)
-    LDR r1, =delay_state
+    LDR r1, =estado_delay
     LDR r1, [r1]
 
 	//Si nos encontramos en el estado de velocidad lento "1"
